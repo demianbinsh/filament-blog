80ed5291d1c2dbe18b7336a2f6fadeafd07ce59c
Demian <demian.shtepa@gmail.com>
2024-05-15 14:25:19 +0300

    Delete events

--- src/EventServiceProvider.php
+++ src/EventServiceProvider.php
@@ -2,17 +2,11 @@
 
 namespace Firefly\FilamentBlog;
 
-use Firefly\FilamentBlog\Events\BlogPublished;
-use Firefly\FilamentBlog\Listeners\SendBlogPublishedNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
 {
-    protected $listen = [
-        BlogPublished::class => [
-            SendBlogPublishedNotification::class,
-        ],
-    ];
+    protected $listen = [];
 
     /**
      * Register any events for your application.
